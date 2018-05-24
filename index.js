03ed1c1ae24ed2a9e0739d648f4dade4223a0abf
Rosa Tu <rosalina.tu@gmail.com>
2018-05-24 15:13:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,16 +29,10 @@ function dec(n)
   
 function makeInt(n) 
 {
-  if(${'n'} = NaN)
-console.log('NaN')
-else
-  return parseInt('n', 10)
+  return parseInt(n, 10)
 }  
 
 function preserveDecimal(n)
 {
-   if(${'n'} = NaN)
-console.log('NaN')
-else
   return parseFloat(n)
 }
